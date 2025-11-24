cd49fb2c8f2781c6718ef66fa9ec99d806a4cf7b
TobiasAagaard <tach@trifork.com>
2025-11-24 14:03:34 +0100

    feat: add CLI argument for dynamic file path selection

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from stats import get_book_text, total_num_words , count_unique_characters, sort_characters_dictonary
+import sys
 
-book = 'books/frankenstein.txt'
+
 
 def main():
-    text = get_book_text(book)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    text = get_book_text(sys.argv[1])
     word_count = total_num_words(text)
     char_count = count_unique_characters(text)
     sorted_chars = sort_characters_dictonary(char_count)
 
     print("============ BOOKBOT ============")
-    print (f"Analyzing book found at {book}")
+    print (f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("-------- Character Count -------")
